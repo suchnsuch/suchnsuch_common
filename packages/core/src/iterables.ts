3b5fcb3003b826be352995c4fdc4ee03aad20d42
Taylor Hadden <taylorhadden@icloud.com>
2022-10-12 09:40:19 -0400

    Doc updates.

--- packages/core/src/iterables.ts
+++ packages/core/src/iterables.ts
@@ -1,3 +1,7 @@
+/**
+ * Pulls the first item from an array, or null.
+ * Handles both empty and null arrays.
+ */
 export function firstOrNull<T>(array: T[]): T | null {
 	return array?.length ? array[0] : null
 }
@@ -6,6 +10,9 @@ export function last<T>(array: T[]): T {
 	return array[array.length - 1]
 }
 
+/**
+ * Checks if the target array includes any item in the source array
+ */
 export function includesAny<T>(target: T[], source: T[]) {
 	for (const i of source) {
 		if (target.includes(i)) {
@@ -22,10 +29,18 @@ export function firstOfType<T, S>(iterator: Iterable<S>, type: new (...args: any
 	return null
 }
 
+/**
+ * If a value is not nullish, returns an array with that value in it.
+ * Otherwise, returns an empty array.
+ */
 export function singleOrEmpty<T>(value: T): T[] {
 	return (value !== null && value !== undefined) ? [value] : []
 }
 
+/**
+ * Removes an item from an array by putting the last item in the array in its place.
+ * Does not require shifting all subsequent items in the array, but does not maintain order.
+ */
 export function swapRemove<T>(array: T[], item: T) {
 	if (array.length === 0) return
 
