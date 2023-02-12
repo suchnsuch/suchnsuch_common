ec28316c1aef131c9e05b381c63d79a12a80a159
Taylor Hadden <taylorhadden@icloud.com>
2023-02-12 11:34:03 -0500

    Adds findFullCharacter Forward & Reverse

--- packages/core/src/strings.ts
+++ packages/core/src/strings.ts
@@ -11,3 +11,74 @@ export function repeatString(text: string, count: number) {
 	return result
 }
 
+// \u200D is a zero-width-joiner
+// \ud800-\udbff is the range for a first surrogate pair
+// \udc00-\udfff is the range for a second surrogate pair
+// See: https://javascript.info/unicode#surrogate-pairs
+const FULL_STEP_MATCH = /[\u200D\ud800-\udbff\udc00-\udfff]/u
+
+/**
+ * Finds the "full character step" (e.g. emoji, compound emoji) from a given index in the given string.
+ * With normal ASCII-like unicode characters, a single character will be returned.
+ * Will return two-characters for surrogate pairs (e.g. ❄️).
+ * Will return many characters for characters combined with \u200D, the zero-lenght joiner.
+ * @param text The input text
+ * @param index The position from which to find the full character step.
+ */
+export function findFullCharacterForward(text: string, index = 0): string | undefined {
+	if (index < 0 || index >= text.length) return undefined
+
+	const char = text[index]
+	if (!char.match(FULL_STEP_MATCH)) {
+		// At a normal character, just stop
+		return char
+	}
+
+	let lastIndex = index + 1
+	while (lastIndex < text.length) {
+		// Check the next character
+		const next = text[lastIndex]
+		if (!next.match(FULL_STEP_MATCH)) {
+			// The next character was totally normal. We're done.
+			break
+		}
+		// The next character is one of a surrogate pair (See: https://javascript.info/unicode#surrogate-pairs)
+		// or is a zero-length-joiner (\u200D) which combines emoji
+		// Either way, we want to consume it
+		lastIndex++
+	}
+
+	return text.substring(index, lastIndex)
+}
+
+/**
+ * Finds the "full character step" (e.g. emoji, compound emoji) just before a given index in the given string.
+ * With normal ASCII-like unicode characters, a single character will be returned.
+ * Will return two-characters for surrogate pairs (e.g. ❄️).
+ * Will return many characters for characters combined with \u200D, the zero-lenght joiner.
+ * @param text The input text
+ * @param index The position from which to find the full character step.
+ */
+export function findFullCharacterReverse(text: string, index: number): string | undefined {
+	if (index < 0 || index >= text.length) return undefined
+
+	let lastIndex = index - 1
+	while (lastIndex >= 0) {
+		// Check the next character
+		const next = text[lastIndex]
+		if (!next.match(FULL_STEP_MATCH)) {
+			// The next character was totally normal. We're done.
+			break
+		}
+		// The next character is one of a surrogate pair (See: https://javascript.info/unicode#surrogate-pairs)
+		// or is a zero-length-joiner (\u200D) which combines emoji
+		// Either way, we want to consume it
+		lastIndex--
+	}
+
+	if (index - lastIndex > 1) {
+		// Need to shift this back forward
+		return text.substring(lastIndex + 1, index)
+	}
+	return text.substring(lastIndex, index)
+}
---
+++ packages/core/tests/strings.test.ts
@@ -0,0 +1,30 @@
+import { findFullCharacterForward, findFullCharacterReverse } from '../src/strings'
+
+describe('findFullCharacter', () => {
+	it('Should return a single character for ASCII-like characters', () => {
+		expect(findFullCharacterForward('test')).toEqual('t')
+		expect(findFullCharacterForward('test', 1)).toEqual('e')
+
+		expect(findFullCharacterReverse('test', 3)).toEqual('s')
+	})
+
+	it('Should return all of a surrogate pair', () => {
+		expect('😉'.length).toEqual(2)
+		expect(findFullCharacterForward('Hi😉there', 2)).toEqual('😉')
+
+		expect(findFullCharacterReverse('Hi😉there', 4)).toEqual('😉')
+	})
+
+	it('Should not continue to consume after a normal character', () => {
+		expect(findFullCharacterForward('Hi 😉 there', 2)).toEqual(' ')
+	})
+
+	it('Should return all of a combined emoji', () => {
+		expect(findFullCharacterForward('👨‍👩‍👧‍👦')).toEqual('👨‍👩‍👧‍👦')
+	})
+
+	it('Should be able to return the first character, backwards', () => {
+		expect(findFullCharacterReverse('hello', 1)).toEqual('h')
+		expect(findFullCharacterReverse('😉hello', 2)).toEqual('😉')
+	})
+})
