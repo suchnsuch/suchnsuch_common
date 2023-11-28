f2e4c9dc202dd0a0485f6e7e7a5a367191cb617e
Taylor Hadden <taylorhadden@icloud.com>
2023-11-28 08:47:10 -0500

    Updates the promise queue with a new ReadWritePromiseQueue.

--- packages/core/src/async.ts
+++ packages/core/src/async.ts
@@ -16,33 +16,35 @@ export function requestCallbackOnIdle(callback: () => void, timeout = 500) {
 
 export type PromiseStarter<T> = () => Promise<T>
 
-type PromiseQueueItem<T> = {
-	starter: PromiseStarter<T>
-	resolve: (result: T | PromiseLike<T>) => void
-	reject: (error: any) => void
+interface PromiseQueueItem<T> {
+	start: PromiseStarter<T>
+	resolve?: (result: T | PromiseLike<T>) => void
+	reject?: (error: any) => void
 }
 
-export class PromiseQueue {
-	tasks: PromiseQueueItem<any>[] = []
-	index: number = -1
-
-	enqueue<T>(starter: PromiseStarter<T>): Promise<T> {
-		const item: Partial<PromiseQueueItem<T>> = {
-			starter
-		}
-
-		// Create a new promise so that this item can resolve when finished
-		const promise = new Promise<T>((resolve, reject) => {
-			item.resolve = resolve
-			item.reject = reject
-		})
-
-		this.tasks.push(item as PromiseQueueItem<T>)
-
-		return promise
+function appendQueueItem<T>(starter: PromiseStarter<T>, tasks: PromiseQueueItem<any>[]): Promise<T> {
+	const item: Partial<PromiseQueueItem<T>> = {
+		start: starter
 	}
 
-	async start() {
+	// Create a new promise so that this item can resolve when finished
+	const promise = new Promise<T>((resolve, reject) => {
+		item.resolve = resolve
+		item.reject = reject
+	})
+
+	tasks.push(item as PromiseQueueItem<T>)
+
+	return promise
+}
+
+class BaseQueue {
+	protected tasks: PromiseQueueItem<any>[] = []
+	protected index: number = -1
+
+	protected _complete: Promise<void> | null = null
+
+	protected async start() {
 		if (this.index < 0) {
 			this.index = 0
 		}
@@ -50,19 +52,153 @@ export class PromiseQueue {
 		while (this.index < this.tasks.length) {
 			const item = this.tasks[this.index]
 	
-			let result;
+			let result
 			try {
-				result = await item.starter()
+				result = await item.start()
 			}
 			catch (e) {
-				item.reject(e)
+				if (item.reject) {
+					item.reject(e)
+				}
 				continue;
 			}
 	
-			item.resolve(result)
+			if (item.resolve) {
+				item.resolve(result)
+			}
 	
 			this.index++
 		}
+
+		this._complete = null
+	}
+
+	get onComplete() { return this._complete }
+}
+
+export class PromiseQueue extends BaseQueue {
+	enqueue<T>(starter: PromiseStarter<T>): Promise<T> {
+		const promise = appendQueueItem(starter, this.tasks)
+
+		if (this._complete === null) {
+			this._complete = this.start()
+		}
+
+		return promise
 	}
 }
+
+enum ReadSetState {
+	Unstarted,
+	Active,
+	Closed
+}
+
+interface ReadSetItem<T> extends PromiseQueueItem<T> {
+	promise: Promise<T>
+}
+
+class ReadSet implements PromiseQueueItem<void> {
+
+	private state: ReadSetState = ReadSetState.Unstarted
+	private tasks: ReadSetItem<any>[] = []
+
+	// Don't expose the resolve, that's for us to call
+	private _resolve?: ((result: void | PromiseLike<void>) => void) | undefined
+
+	appendTask<T>(starter: PromiseStarter<T>): Promise<T> {
+		const promise = appendQueueItem(starter, this.tasks)
+
+		if (this.state === ReadSetState.Active) {
+			this.startItem(this.tasks.at(-1) as ReadSetItem<any>)
+		}
+
+		return promise
+	}
+
+	private startItem(item: ReadSetItem<any>) {
+		item.promise = item.start()
+		.then(result => {
+			if (item.resolve) {
+				item.resolve(result)
+			}
+		})
+		.catch(err => {
+			if (item.reject) {
+				item.reject(err)
+			}
+		})
+		.finally(() => this.finishItem(item))
+	}
+
+	private finishItem(item: ReadSetItem<any>) {
+		const index = this.tasks.indexOf(item)
+		if (index >= 0) {
+			this.tasks.splice(index, 1)
+		}
+
+		if (this._resolve && this.tasks.length === 0) {
+			this._resolve()
+		}
+	}
+
+	start() {
+		return new Promise<void>((resolve, reject) => {
+			this.state = ReadSetState.Active
+			this._resolve = resolve
+
+			for (const task of this.tasks) {
+				this.startItem(task)
+			}
+		})
+	}
+
+	closeout() {
+		if (this.state !== ReadSetState.Active) {
+			console.warn('Closed out read group when it was: ', this.state)
+		}
+
+		this.state = ReadSetState.Closed
+		if (this._resolve && this.tasks.length === 0) {
+			this._resolve()
+		}
+	}
+}
+
+export class ReadWritePromiseQueue extends BaseQueue {
+
+	queueRead<T>(starter: PromiseStarter<T>): Promise<T> {
+		const last = this.tasks.at(-1)
+		if (last instanceof ReadSet) {
+			return last.appendTask(starter)
+		}
+
+		const set = new ReadSet()
+		this.tasks.push(set as PromiseQueueItem<void>)
+
+		const promise = set.appendTask(starter)
+
+		if (this._complete === null) {
+			this._complete = this.start()
+		}
+
+		return promise
+	}
+
+	queueWrite<T>(starter: PromiseStarter<T>): Promise<T> {
+		const last = this.tasks.at(-1)
+		if (last instanceof ReadSet) {
+			last.closeout()
+		}
+
+		const promise = appendQueueItem(starter, this.tasks)
+
+		if (this._complete === null) {
+			this._complete = this.start()
+		}
+
+		return promise
+	}
+}
+
 
--- packages/core/tests/async.test.ts
+++ packages/core/tests/async.test.ts
@@ -1,4 +1,4 @@
-import { wait, PromiseQueue } from '../src/async'
+import { wait, PromiseQueue, ReadWritePromiseQueue } from '../src/async'
 
 describe('Promise Queue', () => {
 	it('Will execute requests, in order', async () => {
@@ -10,15 +10,102 @@ describe('Promise Queue', () => {
 			await wait(100)
 			stuff.push('Hello')
 		})
-		const end = queue.start()
-	
+
 		queue.enqueue(async () => {
 			await wait(10)
 			stuff.push('World')
 		})
 	
-		await end
+		await queue.onComplete
 	
 		expect(stuff).toEqual(['Hello', 'World'])
+	})
+})
+
+describe('Read Write Promise Queue', () => {
+	it('Will execute read tasks simultaneously', async () => {
+		const stuff = new Array<string>()
+		const queue = new ReadWritePromiseQueue();
+
+		queue.queueRead(async () => {
+			await wait(20)
+			stuff.push('World')
+		})
+
+		queue.queueRead(async () => {
+			await wait(10)
+			stuff.push('Hello')
+		})
+
+		await queue.onComplete
+
+		expect(stuff).toEqual(['Hello', 'World'])
+	})
+
+	it('Will execute write tasks in order', async () => {
+		const stuff = new Array<string>()
+		const queue = new ReadWritePromiseQueue();
+
+		queue.queueWrite(async () => {
+			await wait(20)
+			stuff.push('Hello')
+		})
+
+		queue.queueWrite(async () => {
+			await wait(10)
+			stuff.push('World')
+		})
+
+		await queue.onComplete
+
+		expect(stuff).toEqual(['Hello', 'World'])
+	})
+
+	it('Will wait to start write tasks until all read tasks are complete', async () => {
+		const stuff = new Array<string>()
+		const queue = new ReadWritePromiseQueue();
+
+		const one = queue.queueRead(async () => {
+			await wait(50)
+			stuff.push('World')
+		})
+
+		queue.queueRead(async () => {
+			await wait(40)
+			stuff.push('Hello')
+		})
+
+		queue.queueWrite(async () => {
+			await wait(1)
+			stuff.push('Wazzup!?')
+		})
+
+		await queue.onComplete
+
+		expect(stuff).toEqual(['Hello', 'World', 'Wazzup!?'])
+	})
+
+	it('Will start groups of simultaneous reads after a write completes', async () => {
+		const stuff = new Array<string>()
+		const queue = new ReadWritePromiseQueue();
+
+		queue.queueWrite(async () => {
+			await wait(50)
+			stuff.push('Write')
+		})
+
+		queue.queueRead(async () => {
+			await wait(1)
+			stuff.push('Read 1')
+		})
+
+		queue.queueRead(async () => {
+			await wait(2)
+			stuff.push('Read 2')
+		})
+		
+		await queue.onComplete
+
+		expect(stuff).toEqual(['Write', 'Read 1', 'Read 2'])
 	})
 })
